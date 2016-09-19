524b26a066612478c276f8eda61e7668eccd78f3
Derek <dstark85@gmail.com>
2016-09-19 12:52:01 -0700

    barebones interst program that prints a clunky table

---
+++ psets/two/interest.py
@@ -0,0 +1,34 @@
+# This program runs through a calculation of interest rate by month
+
+def compound(charge, min_payment, apr):
+    '''
+    Charge is a float that represents the amount charged to credit card
+    min_payment is a float representing a percentage (0.1 = 10%)
+    apr is a float also representing a percentage (0.1 = 10%)
+    Calculates the new balance for each month
+    return the balance after one year of minimum payments.
+       Can consider how long the debt would take to get rid of.
+    '''
+    def calc_interest(amount, apr):
+        '''
+        Takes amount, apr as floats and returns the amount of interest
+            accrued       
+        '''
+        return 	apr / 12 * amount
+ 
+    new_balance = charge  
+    total_paid = 0
+
+    for i in range(12):
+        payment = new_balance * min_payment
+        total_paid += payment
+        unpaid_balance = new_balance - payment
+        interest = calc_interest(unpaid_balance, apr)
+        print(i, new_balance, payment, unpaid_balance, interest) 
+        new_balance = unpaid_balance + interest
+
+    return total_paid 
+
+
+total = compound(5000, .02, .18)
+print("total amount paid is", total)
