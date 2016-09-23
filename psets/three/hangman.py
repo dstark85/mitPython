afbb43e23ca6bbf52ddaf203dd38ffa81b017170
Derek <dstark85@gmail.com>
2016-09-23 08:51:23 -0700

    complete a working game

--- psets/three/hangman.py
+++ psets/three/hangman.py
@@ -66,28 +66,66 @@ def hangman():
         guess = input('what is your guess: ')
         return guess 
 
+    def process_guess(guess, word, letter_dict):
+        '''
+        guess is a string of length 1 (a letter) or word guess
+        word is a string of the word
+        returns true when the guess is the same as the word, false otherwise
+        
+        ''' 
+        def check_letter(letter, d):
+            '''
+            accepts letter as a one-char length string.
+            d is a dictionary with one-char length strings as keys
+            mutates d based on whether letter is in it or not
+            '''
+            if letter in d.keys():
+                if d[letter]:
+                    print("You already guessed this letter!")
+                    # increment guesses
+                    return None
+                d[letter] = True
 
+        def check_word(guess_word, word):
+            '''
+            takes two strings. Prints msg if equal
+            '''
+            if guess_word == word:
+                print("Congratulations!")
+                # end game
+                return True
+            else:
+                print("That is not the word")
+ 
+
+        if not guess.isalpha():
+            print("The word only contains alphabetical characters")
+            return False
+        
+        if len(guess) == 1:
+            check_letter(guess, letter_dict)   
+        else:
+            return check_word(guess, word)
+            
+        return False
+        
+    lives = 9
     word_list = get_word_list('words.txt')
     not_guessed_letters = list('abcdefghijklmnopqrstuvwxyz')
     word = choose_word(word_list) 
     letter_dict = make_dict(word)
     print_message()
     show_word(word, letter_dict)
-    guess = get_guess()    
+    while lives > 0:
+        guess = get_guess()    
+        if process_guess(guess, word, letter_dict):
+            break
+        show_word(word, letter_dict)
+        lives -= 1
 
+    if lives == 0:
+        print("Sorry, you have lost")
+        print("The word was", word)
 
-def get_guess():
-    '''
-    returns the guess that the user makes
-    '''
-    guess = input('what is your guess: ')
-    return guess 
-
-def check_letter(letter, possible_guesses):
-    '''
-    Take a letter and see if it has been guessed already.
-    Return True if it is a new guess
-    '''
-    return letter in possible_guesses
-            
+           
 hangman()
